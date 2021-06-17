56426865cdf35f96a6897184ba254151e929861a
Aleajandro <a.lazaro.g7@gmail.com>
2021-06-17 16:52:24 -0400

    First Exercise

---
+++ BootCamp/Ejercicio1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BootCamp
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci(2);
+            Console.ReadKey(true);
+        }
+        static void Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+            Console.Write("0 ");
+            for (int i = 1; i < n; i++)
+            {
+                n1 = n2;
+                n2 = sum;
+                sum = n1 + n2;
+                Console.Write($"{sum} ");
+            }
+        }
+    }
+}
