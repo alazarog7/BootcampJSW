106003a8ed7fdc65b7b351adc015f8fa87820f0e
Aleajandro <a.lazaro.g7@gmail.com>
2021-06-22 17:50:40 -0400

    NullValues

---
+++ 03-NullValues/NullValues/NullValues/Program.cs
@@ -0,0 +1,50 @@
+﻿#nullable disable
+using System;
+
+namespace NullValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int x = 0;
+            int? nx = null;
+
+            Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            Console.WriteLine("x == nx: {0}",x==nx);
+            Console.WriteLine("x != nx: {0}", x != nx );
+            
+            nx = 20;
+            
+            Console.WriteLine("nx has a value? {0}", nx.HasValue);
+            Console.WriteLine("nx has the value {0}", nx.Value);
+            Console.WriteLine("x == nx: {0}", x == nx );
+            Console.WriteLine("x != nx: {0}", x != nx);
+            
+            nx = null;
+            Console.WriteLine("nx = {0}", nx ?? -1);
+            Console.WriteLine("nx = {0}", nx.GetValueOrDefault());
+            Console.WriteLine("nx = {0}", nx.GetValueOrDefault(-2));
+            
+            nx = 10;
+            
+            Console.WriteLine("nx = {0}", nx ?? -1 );
+            Console.WriteLine("nx = {0}", nx.GetValueOrDefault());
+            Console.WriteLine("nx = {0}", nx.GetValueOrDefault(-2));
+
+            //string thisCouldBeNull = null;
+            //
+            ////if(thisCouldBeNull != null)
+            ////{
+            ////    int l = thisCouldBeNull.Length;
+            ////}
+            //string authorName = null;
+            //var result = authorName?.Length ?? 3;
+            //Console.WriteLine(result);
+
+
+        }
+    }
+}
