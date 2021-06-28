f4d3b7d4022a126c094a334ee30e67bb71cce61f
Aleajandro <a.lazaro.g7@gmail.com>
2021-06-28 16:27:56 -0400

    Generic Excercise

---
+++ 07-Generics-Part2-Exercise/Generics/Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.Unique.Name = "pepe";
+            //Person.Unique.LastName = "pepe";
+            Console.WriteLine(Person.Unique.Name);
+            //Console.WriteLine(Person.Unique.LastName);
+        }
+    }
+
+    class Person : UniqueInstance<Person>
+    {
+        public string Name;
+    }
+
+    class UniqueInstance<T> where T: new()
+    {
+        public static T Unique = Unique !=null ? Unique : new(); // reference to a instance of a 
+    }
+}
---
+++ 08-AnonymousTypes/AnonymousTypes/AnonymousTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new
+            {
+                Id = 1,
+                FirstName = "Jame",
+                LastName = "Scoot"
+            };
+
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.GetType().Name);
+        }
+    }
+}
